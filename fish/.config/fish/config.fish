62807d337474acd65ff20ad0ac372b726b735680
Erik Zenker <erik.zenker@citrix.com>
2016-10-28 10:16:25 +0200

    Added col env to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,10 @@ setxkbmap us
 set -x LS_COLORS di=36
 set -x VISUAL emacs
 set -x EDITOR $VISUAL
+
+# COL Environment
+set -x COL_LOCAL_BUILDS_ROOT /home/erikz/tmp/
+set -x COL_BUILDS_ROOT https://artifactory.prodwest.citrixsaassbe.net/artifactory/PlatformDependencies/com/getgo/dependencies/
+set -x COL_ARTIFACTORY_ROOT https://artifactory.prodwest.citrixsaassbe.net/artifactory/PlatformDependencies/com/getgo/dependencies/
+set -x COL_COPY_PREBUILT ON
+set -x WITH_COPY_PREBUILT ON
